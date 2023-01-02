c344f9ced5449ba916babf415ec8f861cbfb97ac
ehdrn3020 <ehdrnsla3020@naver.com>
2023-01-03 07:17:42 +0900

    20230103 product map props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import { useState } from "react";
 import { Button, Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
+import data from './data';
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
 
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Brand</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -20,26 +25,26 @@ function App() {
 
       <Container>
         <Row>
-          <Col sm>
-            <img src={process.env.PUBLIC_URL + 'img/shoes1.jpg'} width="90%"></img>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img src={process.env.PUBLIC_URL + 'img/shoes2.jpg'} width="90%"></img>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img src={process.env.PUBLIC_URL + 'img/shoes3.jpg'} width="90%"></img>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
+          { shoes.map((item, i)=>{
+              return (
+                <Product item={item} i={i+1} />
+              )
+          })}
         </Row>
       </Container>
       
     </div>
   );
+}
+
+function Product(props) {
+  return (
+    <Col sm key={props.item.id}>
+      <img src={process.env.PUBLIC_URL + 'img/shoes'+props.i+'.jpg'} width="90%"></img>
+      <h4>{props.item.title}</h4>
+      <p>{props.item.price}</p>
+    </Col>
+  )
 }
 
 export default App;
